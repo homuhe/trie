a55d59fc8d6595436c1d6d1ee59d967eb8c84d0e
harambe4president <al.hartmann@student.uni-tuebingen.de>
2016-11-29 16:13:11 +0100

    test

--- src/main/scala/com/ir/Trie.scala
+++ src/main/scala/com/ir/Trie.scala
@@ -1,13 +1,26 @@
 package com.ir
 
 /**
- * @author ${user.name}
- */
+  * @author ${user.name}
+  */
 object Trie {
 
 
-  def main(args : Array[String]) {
+  def main(args : Array[String]): Unit =  {
     println( "Hello World!" )
   }
 
 }
+
+
+
+class Edge(char: Char, start: Int, end: Int){
+
+
+}
+
+class Node(){
+  val edges: List[Edge] = Nil
+}
+
+
